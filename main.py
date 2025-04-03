ba5142e6c4e1267d1b12fc913d18d6ff3d12068f
Mahdi Jankari <mjankari42@gmail.com>
2025-04-03 17:29:59 +0300

    add initial game loop

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
@@ -5,6 +6,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+    
+    while running:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        
+        screen.fill("black")
+
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
