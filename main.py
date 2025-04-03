1e11c5f8376a75397810ddc34475eacd607fc796
Mahdi Jankari <mjankari42@gmail.com>
2025-04-03 18:13:45 +0300

    add player sprite to middle of screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
+
 
 def main():
     print("Starting Asteroids!")
@@ -11,21 +13,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     running = True
 
     while running:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
