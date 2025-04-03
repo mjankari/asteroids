ac3a3a5925e5e09b1074c959c9b19f9229ebfb5b
Mahdi Jankari <mjankari42@gmail.com>
2025-04-03 17:05:18 +0300

    add constants module

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
